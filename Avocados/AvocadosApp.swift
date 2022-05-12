1b05a5a41180462a7e64022eddc187edfcb00f49
Aman Sharma <shrma.aman7@gmail.com>
2022-05-12 14:39:09 +0530

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by AMAN SHARMA on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
